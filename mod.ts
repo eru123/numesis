5731a3b13e1812df6f3294f73499774d37106a09
jer-echo <aquino.jericho20@gmail.com>
2022-06-16 13:28:16 +0800

    improve performace and readability

--- mod.ts
+++ mod.ts
@@ -1,30 +1,35 @@
 class Numesis {
-	public readonly c: {s:string,l:number,i:number,a: Array<string>};
-	constructor(ch:string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"){
-		this.c = {
-			s: ch,
-			l: ch.length,
-			i: ch.length - 1,
-			a: ch.split("")
-		}
-	}
-	private ep(n:number, c = ""): string {
-		n = Math.floor(n)
-		const {l,i,a} = this.c
-		if(n <= i) return a[n] + c;
-		const m: number = n % l;
-		c = m > 0 && m <= l ? a[m] + c : a[0] + c
-		const d: number = Math.floor(n / l);
-		return this.ep(d,c)
-	}
-	e(n:number): string {
-		const str = String(n)
-		if(str.includes(".")) {
-			const st = str.split(".")
-			return `${this.ep(Number(st[0]) || 0)}.${this.ep(Number(st[1]) || 0)}`
-		}
-		return this.ep(n)
-	}
+    public readonly charset;
+    constructor(chartset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
+        this.charset = Array.from(new Set(chartset.split("")))
+    }
+    private encode_process(number: number | string, current = ""): string {
+        number = parseInt("" + number)                      
+        const length: number = this.charset.length          
+        const lastIndex: number = length - 1                
+        const set: Array<string> = this.charset             
+        if (number <= lastIndex) return "" + (set[number] + current); 
+        const m: number = number % length;                  
+        current = m > 0 && m <= length ? set[m] + current : set[0] + current 
+        const d: number = parseInt((number / length) + ""); 
+        return this.encode_process(d, current)              
+    }
+    ep: Function = this.encode_process
+    encode(...args){
+        return args
+            .map((n) => {
+                const str = String(n)                    
+                if (/\./i.test(str)) {                   
+                    const st = str.split(".")            
+                    
+                    return `${this.encode_process(+st[0] || 0)}.${this.encode_process(+st[1] || 0)}`
+                }
+
+                return this.encode_process(n)
+            })
+            .join("")
+    }
+    e: Function = this.encode.bind(this)
 }
 
 export default Numesis;
